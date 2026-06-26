7a3e733769f38a60149099da66e84899dc9e9da6
Rupesh Bade <rupeshbade246@gmail.com>
2026-06-26 15:18:08 +0545

    Added new base_url

--- config/curl-facade.php
+++ config/curl-facade.php
@@ -8,5 +8,8 @@ return [
         'jsonplaceholder' => [
             'base_url' => env('JSON_API_URL', 'https://jsonplaceholder.typicode.com'),
         ],
+        'mock_api_2' => [
+            'base_url' => env('MOCK_API_2_URL', 'https://6a2912e8f59cb8f65f1c674f.mockapi.io/api/v1'),
+        ],
     ],
 ];
